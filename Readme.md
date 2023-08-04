4ec5d57c32707b52c2b1b3fa20a313288240ed45
Willy Gabriel Vilela <willygabrielnoval@gmail.com>
2023-08-04 12:16:02 -0300

    Complete

---
+++ Readme.md
@@ -0,0 +1,28 @@
+## ☕ Projeto Coffee Delivery
+
+<div align="center">
+<img alt="Capa do projeto" src="public/coffee.gif" width="60%">
+</div>
+
+## 💻 Projeto
+
+Coffee Delivery se trata de uma aplicação de entrega de café, durante esse projeto, foi utilizado conceitos mais aprofundados do ambiente React, como a Context API e estilização com Styled Components, o projeto conta com três telas sendo a primeira uma tela de apresentação e listagens de itens no caso cafés, na segunda tela, o usuário pode preencher um formulário com endereço para entrega que foi construido com React Hook Form e validação com Zod e visualizar seu pedido alterando a quantidade de itens, além de remover os cafés se assim o quiser. Na última tela é exibido uma confirmação do pedido e também os detalhes para entrega.
+
+## 🚀 Tecnologias utilizadas
+
+<ul>
+  <li>Vite</li>
+  <li>Styled Components</li>
+  <li>Typescript</li>
+  <li>Local Storage</li>
+  <li>Context API</li>
+  <li>React Router Dom</li>
+  <li>React Hook Form</li>
+  <li>Zod</li>
+</ul>
+
+## 🔖 Layout do Projeto
+
+O layout pode ser visto através do Figma
+
+[Layout Web](https://www.figma.com/file/5yT9ZzZmRQRS4yivGGB3pl/Coffee-Delivery/duplicate)
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from "./styles";
+import coffeeLogoImg from "../../assets/coffee-delivery-logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
+
+
+
+export function Header() {
+    const { cartQuantity } = useCart();
+
+    
+
+    return (
+        <HeaderContainer >
+            <div className="container">
+                <NavLink to="/">
+                    <img src={coffeeLogoImg} alt="Logo" />
+                </NavLink>
+                <HeaderButtonsContainer>
+                    <HeaderButton variant="purple">
+                        <MapPin size={20} weight="fill" />
+                        Recife, PE
+                    </HeaderButton>
+                    <NavLink to="/completeOrder">
+                        <HeaderButton variant="yellow">
+                            {cartQuantity >= 1 && <span>{cartQuantity}</span>}
+                            <ShoppingCart size={20} weight="fill" />
+                        </HeaderButton>
+                    </NavLink>
+                </HeaderButtonsContainer>
+            </div>
+        </HeaderContainer>
+    )
+}
